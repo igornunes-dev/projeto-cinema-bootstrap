d8a101f7d49f111542133b82abbe6338653e6ab6
Igor Nunes <161251499+igornunes-dev@users.noreply.github.com>
2024-04-10 11:16:33 -0300

    add project

---
+++ src/js/mascaras.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('#numeroCartaoCredito').mask('0000 0000 0000 0000')
+  $('#datavencimentoCredito').mask('00/00')
+  $('#codigosegurancaCredito').mask('000')
+  $('#numeroCartaoDebito').mask('0000 0000 0000 0000')
+  $('#datavencimentoDebito').mask('00/00')
+  $('#codigosegurancaDebito').mask('000')
+})
